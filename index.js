9ff5a1253c4e5b089c1a40727e801c940451570c
bajakprashant1798 <bajakprashant1798@gmail.com>
2026-06-08 01:30:15 +0530

    docker cidi

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const app = express()
 const PORT = process.env.PORT ?? 8000
 
 app.get('/', (req, res) => {
-    res.send("Hello World from Express JS v1.2 with docker")
+    res.send("Hello World from Express JS v2.1 with docker")
 })
 
 app.get('/api', (req, res) => {
-    res.send("Hello World from API v1.2")
+    res.send("Hello World from API v2.1")
 })
 
 app.listen(PORT, () => {
